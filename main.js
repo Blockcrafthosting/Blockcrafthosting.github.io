8c6bdcec9fb03f7f4e02550e34df2756fc2de02a
Blockcrafthosting <blockcrafthostinghosting@gmail.com>
2025-12-22 21:16:17 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function showSection(id) {
+  document.querySelectorAll('.content').forEach(section => {
+    section.classList.remove('active');
+  });
+  document.getElementById(id).classList.add('active');
+}
