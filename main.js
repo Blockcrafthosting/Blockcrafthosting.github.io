16c01dc424b4874a945169ca6b080bdf76161e57
Blockcrafthosting <blockcrafthostinghosting@gmail.com>
2025-12-25 00:17:47 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function showSection(id) {
-  document.querySelectorAll('.content').forEach(section => {
-    section.classList.remove('active');
-  });
-  document.getElementById(id).classList.add('active');
-}
